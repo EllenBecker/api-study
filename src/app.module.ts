cf87d407801ab5a1c7fada219178f667e9bb9cb2
EllenBecker <ellenbeckerdc@gmail.com>
2022-12-07 16:14:22 -0300

    Configurações de conexão com postgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConteudoModule } from './conteudo/conteudo.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.TYPEORM_HOST,
+      port: Number(process.env.TYPEORM_PORT),
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      synchronize: true,
+      entities: [process.env.TYPEORM_ENTITIES]
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
